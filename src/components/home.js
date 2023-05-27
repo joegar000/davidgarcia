562e2dd0b3dff7db1bcb59bf05308784e8fc45d2
David Garcia <joegar000@gmail.com>
2023-05-27 07:54:04 -0500

    Add description for homepage

--- src/components/home.js
+++ src/components/home.js
@@ -2,27 +2,23 @@ import { useParallax } from "react-scroll-parallax";
 import imgMe from "../me.jpg";
 
 export function Home() {
-    // const { ref: refName } = useParallax({
-    //     speed: 75,
-    //     translateX: [100, -100],
-    //     easing: [0, 1, 1, 0]
-    // });
-    // const { ref: refIntro } = useParallax({
-    //     speed: 75,
-    //     translateX: [-100, 100],
-    //     easing: [0, 1, 1, 0]
-    // });
     return (
         <>
             <div className="text-center pt-5 px-2">
                 <span className="m-auto display-6">Hello! My name is David. I'm a software developer.</span>
             </div>
             <div className="row p-2 pt-5">
-                <div className="col-sm d-flex pt-sm-0 pt-5">
-                    <span className="m-auto">some text here</span>
+                <div className="col-md d-flex pt-md-0 pt-5 px-5" style={{ flexDirection: 'column' }}>
+                    <p className="mt-auto px-5" style={{ fontSize: '1.5rem' }}>
+                        Welcome to my personal website! Here's where I post my ideas, reflections, projects, and anything I feel inclined to write about!
+                    </p>
+                    <p className="mb-auto px-5" style={{ fontSize: '1.5rem' }}>
+                        My programming journey began in 2018 when I started college. Since then, I've gotten my bachelors in computer science,
+                        had two internships, and started working full-time.
+                    </p>
                 </div>
-                <div className="col-sm d-flex justify-content-center pt-sm-0 pt-5">
-                    <img className="rounded-5" src={imgMe} style={{ maxWidth: '100%', height: 'auto' }} ></img>
+                <div className="col-md d-flex justify-content-center pt-md-0 pt-5">
+                    <img className="rounded-5" src={imgMe} style={{ maxWidth: '80%', height: 'auto' }} ></img>
                 </div>
             </div >
         </>
