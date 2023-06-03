16a98713fd708dde388c8fa34a6f8addd0e910d3
David Garcia <joegar000@gmail.com>
2023-06-03 07:43:57 -0500

    Wrap react initialization in domcontentloaded event

--- website/src/index.js
+++ website/src/index.js
@@ -5,14 +5,16 @@ import App, { router } from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, RouterProvider } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <RouterProvider router={router}>
-      <App />
-    </RouterProvider>
-  </React.StrictMode>
-);
+document.addEventListener('DOMContentLoaded', () => {
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    <React.StrictMode>
+      <RouterProvider router={router}>
+        <App />
+      </RouterProvider>
+    </React.StrictMode>
+  );
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
