d75ee85968e17b0f35ee0eb6c52c1ee4d8ae5cf7
David Garcia <joegar000@gmail.com>
2023-06-15 22:47:59 -0500

    Fix footer alignment

--- website/src/components/footer.js
+++ website/src/components/footer.js
@@ -1,6 +1,6 @@
 export function Footer() {
     return (
-        <div className="border-top bg-light text-center p-3">
+        <div className="border-top mt-auto bg-light text-center p-3">
             <p>© 2023 David Garcia</p>
         </div>
     )
