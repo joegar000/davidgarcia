809f3eaff464de7ae027a477874915ea3a3eb9cd
David Garcia <joegar000@gmail.com>
2023-06-25 17:56:51 -0500

    Generate RSS when creating or editing post

--- management_app/src/preload.js
+++ management_app/src/preload.js
@@ -1,8 +1,10 @@
 import { LowSync } from "lowdb";
 import { JSONFileSync } from "lowdb/node";
 import { Feed } from "feed";
+import fs from "fs";
 
 const PATH_TO_DATA = __dirname + '/../../website/public/mock-db.json';
+const PATH_TO_RSS = __dirname + '/../../website/public/rss.xml';
 
 const adapter = new JSONFileSync(PATH_TO_DATA);
 const db = new LowSync(adapter, { posts: [], projects: [], drafts: [] });
@@ -14,6 +16,7 @@ window.api = {
             db.data.posts = [];
         callback(db.data.posts);
         db.write();
+        updateRSS();
     },
     getPosts: () => {
         db.read();
@@ -48,7 +51,6 @@ function updateRSS() {
         title: 'David Garcia',
         description: 'The personal blog of David Garcia',
         copyright: 'David Garcia',
-        updated: new Date().toGMTString(),
         id: 'https://joegar000.github.io/davidgarcia/',
         link: 'https://joegar000.github.io/davidgarcia/',
         author: {
@@ -57,13 +59,17 @@ function updateRSS() {
         }
     });
     db.data.posts.forEach((post, i) => {
-        const url = `https://joegar000.github.io/davidgarcia/#/blog/${i}`;
+        const url = `https://joegar000.github.io/davidgarcia/#/blog/${i + 1}`;
         feed.addItem({
             title: post.title,
             id: url,
             link: url,
             description: post.subtitle,
-            date: new Date(post.date).toGMTString()
+            date: new Date(post.date)
         });
     });
+
+    if (fs.existsSync(PATH_TO_RSS))
+        fs.unlinkSync(PATH_TO_RSS);
+    fs.writeFileSync(PATH_TO_RSS, feed.rss2());
 }
