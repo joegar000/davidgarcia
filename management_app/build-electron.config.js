aa97bf2239bb1576eef78bdc9b57fc5707645804
David Garcia <joegar000@gmail.com>
2024-09-18 21:49:42 -0500

    remove old files and setup using vite

--- management_app/build-electron.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    mainEntry: 'src/main.js',
-    preloadEntry: 'src/preload.js',
-    outDir: 'build',
-    mainTarget: 'electron16.0-main',
-    preloadTarget: 'electron16.0-preload',
-}
